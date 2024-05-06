64f631faf37bf3efe92d19f2cc0469e4d900e20c
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-05-06 17:54:24 +0300

    add api

---
+++ src/api/api.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "dda441b2-5c1f-4026-aae6-ffbccdaecd79"
+    }
+
+})
+
+export const usersAPI= {
+    getUsers(currentPage: number, pageSize: number) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            })
+    }
+}
