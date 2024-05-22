8890e154611ce2582ddb4a600afdb6c708231473
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-05-22 19:49:18 +0300

    componentDidUpdate for ProfileStatus

--- src/Components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/Components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -28,6 +28,15 @@ class ProfileStatus extends React.Component<ProfileStatusType> {
             status: e.currentTarget.value
         })
     }
+    componentDidUpdate(prevProps: ProfileStatusType) {
+
+        if(prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
     render() {
         return (
             <div>
