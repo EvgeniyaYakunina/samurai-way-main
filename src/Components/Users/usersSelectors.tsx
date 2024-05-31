06a24cf9e478244a83ace5752023f7f3178d16a2
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-05-31 13:28:23 +0300

    createSelectors

--- src/Components/Users/usersSelectors.tsx
+++ src/Components/Users/usersSelectors.tsx
@@ -1,8 +1,14 @@
 import {AppStateType} from "../../redux/redux-store";
+import {createSelector} from "reselect";
 
 export const getUsersPage=(state: AppStateType)=>{
     return state.usersPage.users
 }
+// export const getUsers = createSelector(getUsersPage, (users)=>{
+//     return users.filter(u => true)
+// }) - более сложный селектор на основе простого
+// для сложных действий с данными без лишних перерисовок
+
 export const getPageSize=(state: AppStateType)=>{
     return state.usersPage.pageSize
 }
