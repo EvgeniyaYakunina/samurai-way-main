86fd2c856543c9008aa348a5e1d1b6b372a8ec69
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-06-04 19:51:41 +0300

    add User component

---
+++ src/Components/Users/User.tsx
@@ -0,0 +1,46 @@
+import s from "./Users.module.css";
+import userPhoto from "../../assets/images/userIcon.png";
+import React from "react";
+import {UserType} from "../../redux/users-reducer";
+import {NavLink} from "react-router-dom";
+import {Pagination} from "../../common/Paginator/Pagination";
+
+type UsersPropsType = {
+    user: UserType
+    followTC: (userId: number) => void
+    unfollowTC: (userId: number) => void
+    followingInProgress: Array<number>
+}
+
+export const User = ({user, unfollowTC, followTC, followingInProgress}: UsersPropsType) => {
+
+    return <div>
+        <div key={user.id}>
+               <span>
+                   <div>
+                       <NavLink to={'/profile/' + user.id}>
+                       <img src={user.photos.small != null ? user.photos.small : userPhoto} className={s.userPhoto}/>
+                       </NavLink>
+                   </div>
+                   <div>
+                       {user.followed ?
+                           <button disabled={followingInProgress.some(id => id === user.id)} onClick={() => {
+                               unfollowTC(user.id)
+                           }}> Unfollow </button>
+
+                           : <button disabled={followingInProgress.some(id => id === user.id)} onClick={() => {
+                               followTC(user.id)
+                           }}> Follow </button>}
+                   </div>
+               </span>
+                    <span>
+                   <span>
+                       <div>{user.name}</div><div>{user.status}</div>
+                   </span>
+                   <span>
+                       <div>{'u.location.country'}</div><div>{'u.location.city'}</div>
+                   </span>
+               </span>
+                </div>
+    </div>
+}
